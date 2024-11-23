325b8b0c896b4478776d56f92bf87a0fa38f6275
vlad <vladsebastian.dobrescu@gmail.com>
2024-11-23 14:53:01 +0100

    conan does not support cmake

--- conanfile.py
+++ conanfile.py
@@ -1,20 +1,25 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import copy
 
-print(ConanFile)
 class ImmuDS(ConanFile):
     name = "immuds"
     version = "1.0.0"
     license = "MIT"
     author = "Vlad Dobrescu <dobrescu.vlad10@gmail.com>"
-    url = "https://github.com/yourusername/ImmuDS"
+    url = "https://github.com/vlad-dobrescu/ImmuDS"
     description = "A C++ library of immutable data structures"
     topics = ("c++", "immutable", "data structures")
-    settings = "os", "compiler", "build_type", "arch" 
-    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "gtest/1.10.0"  
+    generators = "CMakeToolchain", "CMakeDeps"
     
+    def layout(self):
+        self.folders.build = "build"
+        self.folders.generators = "build/generators"
+
     def source(self):
-        self.run("git clone https://github.com/vlad-dobrescu/ImmuDS.git")
+        copy(self, "*", self.source_folder, self.build_folder)
 
     def build(self):
         cmake = CMake(self)
